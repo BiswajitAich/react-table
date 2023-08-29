6851d9fc351baf2ef808275c6cc6dda3cd5ca5ba
BISWAJIT AICH <121601439+BiswajitAich@users.noreply.github.com>
2023-08-29 13:24:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import './App.css'
 import Nav from "./Nav";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom"; // Use HashRouter
 import Table from "./Table";
 import TableInput from "./TableInput";
 import Update from "./Update";
 import Home from "./Home";
 
-
 export default function App() {
   return (
     <main>
-      <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/TableInput" element={<TableInput/>} />
-        <Route path="/Table" element={<Table/>} />
-        <Route path="/Update/:index" element={<Update/>} />
-          </Routes>
-        </BrowserRouter>
+      <HashRouter> 
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/TableInput" element={<TableInput />} />
+          <Route path="/Table" element={<Table />} />
+          <Route path="/Update/:index" element={<Update />} />
+        </Routes>
+      </HashRouter>
     </main>
   )
 }
